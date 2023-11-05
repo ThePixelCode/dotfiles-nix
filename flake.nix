71de63f6c513cbd07cc5e258316552ba6b32c0c5
ThePixelCode <thepixelcode@proton.me>
2023-11-05 13:32:30 -0500

    created flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    flatpaks.url = "github:GermanBread/declarative-flatpak/stable";
+    # Please DO NOT override the "nixpkgs" input!
+    # Overriding "nixpkgs" is unsupported unless stated otherwise.
+  };
+
+  outputs = { flatpaks }: {
+    nixosConfigurations.<host> = nixpkgs.lib.nixosSystem {
+      modules = [
+        flatpaks.nixosModules.default
+
+        ./configuration.nix
+      ];
+    };
+  };
+}
