9cd740f475932383d6bc64422b6973cbe2b19dca
ThePixelCode <thepixelcode@proton.me>
2023-11-14 23:07:41 -0500

    flatpaks: added a few flatpaks

--- flatpaks.nix
+++ flatpaks.nix
@@ -1,4 +1,4 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
   config.services.flatpak = {
@@ -7,6 +7,13 @@
     deduplicate = true;
     packages = [
       "flathub:app/com.brave.Browser/x86_64/stable"
+      "flathub:app/com.github.tchx84.Flatseal/x86_64/stable"
+      "flathub:app/com.bitwarden.desktop/x86_64/stable"
+      "flathub:app/com.discordapp.Discord/x86_64/stable"
+      "flathub:app/org.telegram.desktop/x86_64/stable"
+      "flathub:app/com.ktechpit.whatsie/x86_64/stable"
+      "flathub:app/com.ktechpit.torrhunt/x86_64/stable"
+      "flathub:app/org.libreoffice.LibreOffice/x86_64/stable"
     ];
     remotes = {
       "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
