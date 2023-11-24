c862ad61db703d8a7cb366f97b86ebc67fe0b669
ThePixelCode <thepixelcode@proton.me>
2023-11-24 15:33:42 -0500

    flatpaks + flake: Temporaly removed flatpak control

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
   inputs = {
-    flatpaks.url = "github:GermanBread/declarative-flatpak/stable";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # flatpaks.url = "github:GermanBread/declarative-flatpak/stable";
+    # home-manager = {
+    #   url = "github:nix-community/home-manager/release-23.05";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = inputs@{ flatpaks, nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: { # flatpaks, nixpkgs, home-manager, ... }: {
     nixosConfigurations."ShiroPC" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        flatpaks.nixosModules.default
+        # flatpaks.nixosModules.default
 
         ./configuration.nix
         home-manager.nixosModules.home-manager
--- flatpaks.nix
+++ flatpaks.nix
@@ -4,20 +4,21 @@
   config.services.flatpak = {
     # Flatpak
     enable = true;
-    deduplicate = true;
-    packages = [
-      "flathub:app/com.brave.Browser/x86_64/stable"
-      "flathub:app/com.github.tchx84.Flatseal/x86_64/stable"
-      "flathub:app/com.bitwarden.desktop/x86_64/stable"
-      "flathub:app/com.discordapp.Discord/x86_64/stable"
-      "flathub:app/org.telegram.desktop/x86_64/stable"
-      "flathub:app/com.ktechpit.whatsie/x86_64/stable"
-      "flathub:app/com.ktechpit.torrhunt/x86_64/stable"
-      "flathub:app/org.libreoffice.LibreOffice/x86_64/stable"
-      "flathub:app/org.fedoraproject.MediaWriter/x86_64/stable"
-    ];
-    remotes = {
-      "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-    };
+    # deduplicate = true;
+    # packages = [
+    #   "flathub:app/com.brave.Browser/x86_64/stable"
+    #   "flathub:app/com.github.tchx84.Flatseal/x86_64/stable"
+    #   "flathub:app/com.bitwarden.desktop/x86_64/stable"
+    #   "flathub:app/com.discordapp.Discord/x86_64/stable"
+    #   "flathub:app/org.telegram.desktop/x86_64/stable"
+    #   "flathub:app/com.ktechpit.whatsie/x86_64/stable"
+    #   "flathub:app/com.ktechpit.torrhunt/x86_64/stable"
+    #   "flathub:app/org.libreoffice.LibreOffice/x86_64/stable"
+    #   "flathub:app/org.fedoraproject.MediaWriter/x86_64/stable"
+    #   "flathub:app/net.lutris.Lutris/x86_64/stable"
+    # ];
+    # remotes = {
+    #   "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+    # };
   };
 }
